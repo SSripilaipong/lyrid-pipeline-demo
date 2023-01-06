0e0683d9e880030b47f8348ce09781e658db24b9
Santhapon Sripilaipong <SHSnail@mail.com>
2023-01-06 19:38:12 +0700

    feat(url_repo.full.subscribe): test_should_acknowledge_subscription
    Refs: #2

---
+++ tests/url_repo/full/test_subscribe.py
@@ -0,0 +1,6 @@
+from tests.url_repo.base.assertion import assert_acknowledge_subscription
+from tests.url_repo.full.factory import create_full_url_repo
+
+
+def test_should_acknowledge_subscription():
+    assert_acknowledge_subscription(create_full_url_repo())
