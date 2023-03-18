a00e66500aa56ea1d3fa2d605934d0f6bc93c2d1
SSripilaipong <SHSnail@mail.com>
2023-03-18 21:34:08 +0700

    refactor(tests.url_repo.base.assertion): remove unused assertions
    Refs: #2

--- tests/url_repo/base/assertion.py
+++ tests/url_repo/base/assertion.py
@@ -1,15 +1,7 @@
 from lyrid import Address
 from lyrid.testing import ActorTester, CapturedMessage
 
-from demo.core.url_repo import AddUrl, SubscribeUrlData, SubscribeUrlDataAck
-
-
-def assert_not_acknowledge_add_url(tester: ActorTester):
-    consumer = Address("$.someone")
-
-    tester.simulate.tell(AddUrl("https://example.com/1", ref_id="ref123"), by=consumer)
-
-    assert tester.capture.get_messages() == []
+from demo.core.url_repo import SubscribeUrlData, SubscribeUrlDataAck
 
 
 def assert_acknowledge_subscription(tester: ActorTester):
