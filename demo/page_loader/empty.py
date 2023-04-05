2513e4c009aa4fc2adec7c5fa17ee0d0cc64243c
Santhapon Sripilaipong <SHSnail@mail.com>
2023-04-05 20:14:59 +0700

    feat(demo.page_loader.empty.receive_url_data): test_should_run_page_loading_background_task_when_receiving_url_data
    Refs: #2

--- demo/page_loader/empty.py
+++ demo/page_loader/empty.py
@@ -13,21 +13,16 @@ from demo.page_loader.base import PageLoaderBase
 @dataclass
 class EmptyPageLoader(PageLoaderBase):
     waiters: Deque[Address] = field(default_factory=deque)
-    is_loading: bool = False
 
     @switch.message(type=UrlData)
     def receive_url_data(self, message: UrlData):
-        if not self.is_loading:
-            self._run_load_page_in_background(message.url)
-            self.is_loading = True
+        self._run_load_page_in_background(message.url)
 
     @switch.background_task_exited(exception=None)
     def page_loading_completed(self, result: PageData):
         if len(self.waiters) > 0:
             waiter = self.waiters.popleft()
             self.tell(waiter, result)
-
-        self.is_loading = False
 
     @switch.message(type=GetPage)
     def get_page(self, sender: Address):
--- tests/page_loader/empty/test_receive_url_data.py
+++ tests/page_loader/empty/test_receive_url_data.py
@@ -1,6 +1,4 @@
-from lyrid import Address
-
-from tests.page_loader.action import receive_url_data, page_loading_completed
+from tests.page_loader.action import receive_url_data
 from tests.page_loader.empty.factory import create_empty_page_loader, _default_load_page
 
 
@@ -10,31 +8,6 @@ def test_should_run_page_loading_background_task_when_receiving_url_data():
     receive_url_data(tester, url="https://example.com/123")
 
     _assert_have_run_default_loading_background_task(tester, "https://example.com/123")
-
-
-def test_should_not_run_new_page_loading_task_if_existing_task_is_not_completed_yet():
-    url_repo = Address("$.tester.r")
-    tester = create_empty_page_loader(url_repo=url_repo)
-
-    receive_url_data(tester)  # run one task first
-    tester.capture.clear_background_tasks()
-
-    receive_url_data(tester)
-
-    assert len(tester.capture.get_background_tasks()) == 0
-
-
-def test_should_run_new_page_loading_task_immediately():
-    url_repo = Address("$.tester.r")
-    tester = create_empty_page_loader(url_repo=url_repo, load_page=_default_load_page)
-
-    receive_url_data(tester)
-    page_loading_completed(tester)
-    tester.capture.clear_background_tasks()
-
-    receive_url_data(tester, url="https://example.com/1")
-
-    _assert_have_run_default_loading_background_task(tester, "https://example.com/1")
 
 
 def _assert_have_run_default_loading_background_task(tester, url):
