c37ecf055199b934246f038d9cedcbd407d451aa
SSripilaipong <SHSnail@mail.com>
2023-04-06 17:05:28 +0700

    refactor(tests.page_loader.empty.behavior): simplify test case
    Refs: #2

--- tests/page_loader/empty/test_behavior.py
+++ tests/page_loader/empty/test_behavior.py
@@ -2,7 +2,7 @@ from lyrid import Address
 from lyrid.testing import CapturedMessage
 
 from demo.core.page_loader import PageData
-from tests.page_loader.action import receive_url_data, get_page, page_loading_completed
+from tests.page_loader.action import receive_url_data, page_loading_completed
 from tests.page_loader.empty.assertion import _assert_have_run_loading_background_task
 from tests.page_loader.empty.factory import create_empty_page_loader, _default_load_page
 
@@ -18,8 +18,6 @@ def test_should_run_page_loading_background_task_when_receiving_url_data():
 def test_should_send_loaded_page_to_an_existing_waiter_first():
     existing_waiter = Address("$.tester.me")
     tester = create_empty_page_loader(waiters=[existing_waiter])
-
-    get_page(tester, sender=existing_waiter)
 
     page_loading_completed(tester, page=PageData("https://example.com/1", "<html>Done!</html>"))
 
