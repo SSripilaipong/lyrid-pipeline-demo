95f2dce32d3bf4d15b1c1e32a873455dfd86a41d
SSripilaipong <SHSnail@mail.com>
2023-04-08 18:56:14 +0700

    refactor(tests.page_loader.maintain_data): test_should_maintain_waiters_between_empty_state_and_active_state
    Refs: #2

--- tests/page_loader/test_maintain_data.py
+++ tests/page_loader/test_maintain_data.py
@@ -2,6 +2,7 @@ from lyrid.testing import CapturedMessage
 
 from tests.page_loader.action import page_loading_completed, get_page
 from tests.page_loader.active.factory import create_active_page_loader
+from tests.page_loader.empty.factory import create_empty_page_loader
 from tests.page_loader.idle.factory import create_idle_page_loader
 from tests.page_loader.util import random_page_data
 from tests.util import random_address
@@ -14,7 +15,7 @@ def test_should_maintain_loaded_pages_between_active_state_and_full_state():
     page_loading_completed(tester)  # become full
     tester.capture.clear_messages()
 
-    get_page(tester, sender=(first_consumer := random_address()))
+    get_page(tester, sender=(first_consumer := random_address()))  # become active
     assert CapturedMessage(first_consumer, first_page) in tester.capture.get_messages()
     tester.capture.clear_messages()
 
@@ -30,3 +31,20 @@ def test_should_maintain_waiters_between_idle_state_and_empty_state():
     page_loading_completed(tester, page=(loaded_page := random_page_data()))
 
     assert CapturedMessage(existing_waiter, loaded_page) in tester.capture.get_messages()
+
+
+def test_should_maintain_waiters_between_empty_state_and_active_state():
+    tester = create_empty_page_loader(waiters=[random_address()])
+    page_loading_completed(tester)  # for existing waiter
+
+    page_loading_completed(tester, page=(first_page := random_page_data()))  # become active
+    tester.capture.clear_messages()
+
+    get_page(tester, sender=(first_consumer := random_address()))
+    assert CapturedMessage(first_consumer, first_page) in tester.capture.get_messages()
+
+    get_page(tester, sender=(second_consumer := random_address()))  # become empty
+    tester.capture.clear_messages()
+
+    page_loading_completed(tester, page=(second_page := random_page_data()))
+    assert CapturedMessage(second_consumer, second_page) in tester.capture.get_messages()
