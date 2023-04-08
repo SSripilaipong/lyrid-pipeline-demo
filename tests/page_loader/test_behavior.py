ef42bbac7ff1466c121e62b22e50b3a50a18bb56
SSripilaipong <SHSnail@mail.com>
2023-04-08 18:50:57 +0700

    refactor(tests.page_loader.maintain_data): test_should_maintain_waiters_between_idle_state_and_empty_state
    Refs: #2

--- tests/page_loader/test_behavior.py
+++ tests/page_loader/test_behavior.py
@@ -3,19 +3,8 @@ from lyrid.testing import CapturedMessage
 from tests.page_loader.action import get_page, page_loading_completed
 from tests.page_loader.active.factory import create_active_page_loader
 from tests.page_loader.empty.factory import create_empty_page_loader
-from tests.page_loader.idle.factory import create_idle_page_loader
 from tests.page_loader.util import random_page_data
 from tests.util import random_address
-
-
-def test_should_send_loaded_page_to_an_existing_waiter_asked_since_idle_state_when_being_in_empty_state():
-    tester = create_idle_page_loader()
-
-    get_page(tester, sender=(existing_waiter := random_address()))  # become empty
-
-    page_loading_completed(tester, page=(loaded_page := random_page_data()))
-
-    assert CapturedMessage(existing_waiter, loaded_page) in tester.capture.get_messages()
 
 
 def test_should_send_loaded_page_to_an_existing_waiter_asked_since_active_state_when_being_in_empty_state():
--- tests/page_loader/test_maintain_data.py
+++ tests/page_loader/test_maintain_data.py
@@ -2,6 +2,7 @@ from lyrid.testing import CapturedMessage
 
 from tests.page_loader.action import page_loading_completed, get_page
 from tests.page_loader.active.factory import create_active_page_loader
+from tests.page_loader.idle.factory import create_idle_page_loader
 from tests.page_loader.util import random_page_data
 from tests.util import random_address
 
@@ -19,3 +20,13 @@ def test_should_maintain_loaded_pages_between_active_state_and_full_state():
 
     get_page(tester, sender=(second_consumer := random_address()))
     assert CapturedMessage(second_consumer, second_page) in tester.capture.get_messages()
+
+
+def test_should_maintain_waiters_between_idle_state_and_empty_state():
+    tester = create_idle_page_loader()
+
+    get_page(tester, sender=(existing_waiter := random_address()))  # become empty
+
+    page_loading_completed(tester, page=(loaded_page := random_page_data()))
+
+    assert CapturedMessage(existing_waiter, loaded_page) in tester.capture.get_messages()
