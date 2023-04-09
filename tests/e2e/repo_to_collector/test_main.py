92c2498edae7ae61de823dd02f73a8daea69ca32
SSripilaipong <SHSnail@mail.com>
2023-04-09 22:37:03 +0700

    test(e2e.repo_to_collector): add test case to send data from repo to collector via adapter
    Refs: #2

---
+++ tests/e2e/repo_to_collector/test_main.py
@@ -0,0 +1,43 @@
+import multiprocessing as mp
+import time
+
+import pytest
+from lyrid import ActorSystem
+
+from demo.core import common
+from demo.core.url_repo import AddUrls
+from demo.result_adapter import create_result_adapter_from_url
+from demo.result_collector import create_result_collector
+from demo.url_repo import create_url_repo
+from tests.util import random_url
+
+
+@pytest.mark.e2e
+def test_main():
+    result_queue = mp.Manager().Queue()
+    system = ActorSystem()
+
+    try:
+        url_repo = system.spawn(create_url_repo(buffer_size=10), initial_message=common.Start())
+        adapter = system.spawn(create_result_adapter_from_url(url_repo))
+        system.spawn(create_result_collector(adapter, buffer_size=2, save=result_queue.put),
+                     initial_message=common.Start())
+
+        system.tell(url_repo, AddUrls([random_url() for _ in range(20)]))
+        time.sleep(0.1)
+
+        collected_results = []
+        while len(collected_results) < 10:
+            for result in result_queue.get(timeout=3):
+                collected_results.append(result)
+
+        system.tell(url_repo, AddUrls([random_url() for _ in range(20)]))
+
+        collected_results = []
+        while len(collected_results) < 10:
+            for result in result_queue.get(timeout=3):
+                print(result)
+                collected_results.append(result)
+
+    finally:
+        system.force_stop()
